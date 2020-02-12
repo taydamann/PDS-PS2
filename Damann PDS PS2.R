cca1b30195eae209c77fc324749fde8bfdf57b2f
Taylor Damann <taydamann@yahoo.com>
2020-02-11 18:15:27 -0600

    Initial Commit!
    This is the first section of my work in this problem set.

--- Damann PDS PS2.R
+++ Damann PDS PS2.R
@@ -1,21 +1,44 @@
+###TAYLOR J. DAMANN###
+#### PDS PS2 #####
+
+
 #1
+#In a for loop, we put in an argument that tells it how long to loop. I set this
+#to loop from 1 to 7. Following the curly brackets, a for loop needs to know what
+#to do, so I instruct it to print the cube of each number that it iterates over. 
 for(i in 1:7){
   print(i^3)
 }
 
 #2
+#I set the seed at 14 first so that my answers are the same as the rest of my
+#classmates' answers. Then, I ask the for loop to iterate from 1 to 1000 because
+#the question asks for 1000 simulations. Each iteration will be a single simulation.
+#Telling the for loop what to do, I ask it to sample two numbers with replacement
+#from 1 to 6, the numbers on the faces of dice. Now I add some conditional statements.
+#If the two numbers sum to between 8 and 12 inclusively, the game ends. I make an if
+#statement to do this, asking for the sum of each specific iteration. If the sum is
+#between 8 and 12, the function breaks and prints out the values. If not, the iterations
+#continue until a 2 or a 6 is rolled or there have been 1000 iterations. 
 set.seed(14)
+totalRoll <- NULL
+rolls <- NULL
 for(i in 1:1000){
-  x <- sample(1:6, size=2, replace=TRUE)
-  if (sum(i[1])== 8:12) {
-    print(i[1])
-  break
-  } else if (x[i] == 2 | x[i] == 6) {
-    print(x[1]:x[i])
-  } else print(x)
+  x[i] <- sample(1:6, 1)
+  y[i] <- sample(1:6, 1)
+  rolls <- c(rolls, x[i], y[i])
+  firstSum <- x[1]+y[1]
+  totalRoll <- c(totalRoll, x[i]+y[i])
+  if (firstSum == 8 | firstSum == 9 | firstSum == 10 | firstSum == 11 | firstSum == 12) {
+    print("Sum of first roll was between 8 and 12")
+    break
+  } else if (x[i] == 2 | x[i] == 6 | y[i] == 2 | y[i] == 6) {
+    print("One roll equaled 2 or 6")
+    print(paste("The average number of dice casts per game is", mean(totalRoll)))
+    print(c(rolls))
+    break
+  }
 }
-
-
 
 
 
